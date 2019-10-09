52a91fe1d03c32e4a8d2e51a9541bc5b81026faf
Karl Podger <karl.podger@primeordinal.com>
2019-10-09 11:36:52 +0100

    Add plugins to Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,38 @@
-module.exports = {
+const config = {
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        // jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/img/logo.svg`,
+      },
+    },
+    `gatsby-plugin-offline`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
@@ -59,6 +87,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-robots-txt`,
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
@@ -75,3 +104,17 @@ module.exports = {
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
+
+if(process.env.DATABASE_URI1) {
+  config.plugins.push({
+    resolve: "gatsby-source-pg",
+    options: {
+      appendPlugins: [require("@graphile-contrib/pg-simplify-inflector"), require("@graphile-contrib/pg-many-to-many")],
+      connectionString: process.env.DATABASE_URI1,
+      refetchInterval: 60, // Refetch data every 60 seconds
+      schema: "forum_example",
+    },
+  });
+}
+
+module.exports = config;
